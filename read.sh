5b58c214c68ace82a347af9f312a78a79bddf222
prayas2409 <prayas.hemraj.pagade@gmail.com>
2020-05-15 09:54:34 +0530

    Changed color of text from blue to red

--- read.sh
+++
@@ -1,8 +0,0 @@
-read -p "Enter a number" num
-start=$(date +"%T")
-echo $start
-read -p "Enter a number" num
-endtime=$(date +"%T")
-echo $endtime
-timeElapsed=$(($endtime-$start))
-echo "Time elapsed from Start to Stop is:" $timeElapsed
