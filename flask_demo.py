1edb5187298bbb6f9eeca71450b295ce02c2873c
prayas2409 <prayas.hemraj.pagade@gmail.com>
2020-05-21 14:18:01 +0530

    Changed the contents to part 8

--- flask_demo.py
+++ flask_demo.py
@@ -4,8 +4,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def greeting():
-    return """<center><h1 style='color:red'>Welcome to Jenkins Tutorial Part 4</h1>
-		<b><p>This is jenkins demo
+    return """<center><h1 style='color:red'>Welcome to Jenkins Tutorial Part 9</h1>
+		<b><p>This is Jenkins installation demo
 		</b>
 	</center>"""
 
