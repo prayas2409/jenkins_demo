25a116bbea87172f1822e02f7107785171f04203
prayas2409 <prayas.hemraj.pagade@gmail.com>
2020-05-21 14:26:06 +0530

    Changed the contents to installation of Jenkins with webhooks

--- flask_demo.py
+++ flask_demo.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def greeting():
     return """<center><h1 style='color:red'>Welcome to Jenkins Tutorial Part 9</h1>
-		<b><p>This is installation of Jenkins
+		<b><p>This is installation of Jenkins with github webhook
 		</b>
 	</center>"""
 
