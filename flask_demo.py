fd5f7e8f93bffeb4c3b2f63144cb49b2f34d9e5d
prayas2409 <prayas.hemraj.pagade@gmail.com>
2020-08-04 14:43:56 +0530

    Changed the color of contents to red

--- flask_demo.py
+++ flask_demo.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def greeting():
-    return """<center><h1 style='color:blue'>Welcome to Jenkins Tutorial Part 9</h1>
+    return """<center><h1 style='color:red'>Welcome to Jenkins Tutorial Part 9</h1>
 		<b><p>This is installation of Jenkins with github webhook
 		</b>
 	</center>"""
